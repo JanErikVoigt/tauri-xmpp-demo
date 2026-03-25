4579841f64b30f7ff5d997123b933ecbd3aebbc2
Jan-Erik Voigt <mail@janerikvoigt.de>
2026-03-25 16:21:53 +0000

    cargo new tauri app; android init

---
+++ tauri-xmpp-demo/src-tauri/gen/android/app/src/main/java/de/janerikvoigt/tauri_xmpp_demo/MainActivity.kt
@@ -0,0 +1,11 @@
+package de.janerikvoigt.tauri_xmpp_demo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
